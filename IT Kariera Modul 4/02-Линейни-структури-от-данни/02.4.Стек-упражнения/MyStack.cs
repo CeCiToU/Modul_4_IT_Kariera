44ab9b69f5139289a3fae4776558a1093f0a1f51
Tsvetoslav <cveti3060@gmail.com>
2022-06-25 23:06:44 +0300

    Stack is implemented

---
+++ IT Kariera Modul 4/02-Линейни-структури-от-данни/02.4.Стек-упражнения/MyStack.cs
@@ -0,0 +1,112 @@
+﻿namespace ConsoleApp313
+{
+    internal class MyStack<T>
+    {
+
+        private int count;
+        private Node<T> top;
+
+        public MyStack<T> Push(T value)
+        {
+            Node<T> node = new Node<T>(value);
+            if (top == null)
+            {
+                top = node;
+            }
+            else
+            {
+                node.Next = top;
+                top = node;
+            }
+            Count++;
+            return this;
+        }
+        public T Pop()
+        {
+            CheckIfEmpty();
+            T value = top.Value;
+            top = top.Next;
+            Count--;
+            return value;
+        }
+        public T Peek()
+        {
+            CheckIfEmpty();
+            T result = top.Value;
+            return top.Value;
+        }
+        public int StackCount()
+        {
+            return Count;
+        }
+        public void Clear()
+        {
+            Node<T> currentNode = top;
+            while (top != null)
+            {
+                top = null;
+                currentNode.Next = null;
+                currentNode = currentNode.Next;
+            }
+        }
+        public bool Contains(T value)
+        {
+            Node<T> currentNode = top;
+            while (currentNode != null)
+            {
+                if (currentNode.Value.Equals(value))
+                {
+                    return true;
+                }
+                currentNode = currentNode.Next;
+            }
+            return false;
+        }
+        public T[] ToArray()
+        {
+            CheckIfEmpty();
+            Node<T> node = top;
+            T[] array = new T[Count];
+            for(int i = 0; i < Count; i++)
+            {
+                array[i] = node.Value;
+                node = node.Next;
+            }
+            return array;
+        }
+
+        public int Count { get => count; private set => count = value; }
+        private Node<T> Top { get => top; set => top = value; }
+
+        class Node<T>
+        {
+            private T value;
+            private Node<T> next;
+
+            public T Value { get => value; set => this.value = value; }
+            public Node<T> Next { get => next; set => next = value; }
+
+            public Node(T value, Node<T> prevNode)
+            {
+                this.value = value;
+                prevNode.next = this;
+            }
+            public Node(T value)
+            {
+                this.value = value;
+            }
+        }
+        public MyStack()
+        {
+
+        }
+
+        private void CheckIfEmpty()
+        {
+            if (top == null)
+            {
+                throw new System.Exception("Queue is empty!");
+            }
+        }
+    }
+}
---
+++ IT Kariera Modul 4/02-Линейни-структури-от-данни/02.4.Стек-упражнения/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ConsoleApp313
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack<int> stack = new MyStack<int>();
+
+            stack.Push(5);
+            stack.Push(4);
+            stack.Push(8);
+            stack.Push(2);
+            stack.Push(1);
+
+            int[] arr = stack.ToArray();
+
+            Console.WriteLine("Array 1: " + string.Join(", ", arr));
+            Console.WriteLine();
+            Console.WriteLine("Peek = " + stack.Peek());
+            Console.WriteLine("Count = " + stack.StackCount());
+            Console.WriteLine();
+
+            stack.Pop();
+            stack.Pop();
+            int[] array = stack.ToArray();
+
+            Console.WriteLine("Array 2: " + string.Join(", ", array));
+            Console.WriteLine();
+            Console.WriteLine("Peek = " + stack.Peek());
+            Console.WriteLine("Count = " + stack.StackCount());
+            Console.WriteLine();
+            Console.WriteLine("Does it contains \"2\"? " + stack.Contains(2));
+            Console.WriteLine();
+
+            stack.Clear();
+
+            Console.WriteLine("Does it still contains \"2\"? " + stack.Contains(2));
+            Console.WriteLine();
+
+            int[] array3 = stack.ToArray();
+
+            Console.WriteLine("Array 3: " + string.Join(", ", array));
+        }
+    }
+}
