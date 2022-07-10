e2e70be46ece8cc02b12eee9efa0eb149368d4cc
Tsvetoslav <cveti3060@gmail.com>
2022-07-10 10:20:31 +0300

    ?

---
+++ IT Kariera Modul 4/ИЗПИТ_2022/Solution/Book.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Program
+{
+    internal class Book
+    {
+        private string title;
+
+        private double rating;
+
+        public Book(string title, double rating)
+        {
+            Title = title;
+            Rating = rating;
+        }
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        public double Rating
+        {
+
+            get { return rating; }
+            set { rating = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"Book {title} is with {rating} rating.";
+        }
+        public Book()
+        {
+        }
+
+        internal string[] ToArray()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ IT Kariera Modul 4/ИЗПИТ_2022/Solution/BookLibrary.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Program
+{
+
+    internal class BookLibrary
+    {
+        private string name;
+
+        private List<Book> books = new List<Book>();
+
+        public BookLibrary(string name)
+        {
+
+
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+
+        }
+
+        public List<Book> Books
+        {
+            get { return books; }
+            set { books = value; }
+
+        }
+
+        public void AddBook(string title, double rating)
+        {
+            Book currentBook = new Book(title, rating);
+            books.Add(currentBook);
+        }
+
+        public double AverageRating()
+        {
+            double num = 0;
+            for (int i = 0; i < books.Count; i++)
+            {
+                num += books[i].Rating;
+            }
+            num /= books.Count;
+            return num;
+
+        }
+
+        public List<string> GetBooksByRating(double rating)
+        {
+            List<string> titles = new List<string>();
+            for (int i = 0; i < books.Count; i++)
+            {
+                if (books[i].Rating > rating)
+                {
+                    titles.Add(books[i].Title);
+                }
+            }
+            return titles;
+        }
+
+        public List<Book> SortByTitle()
+        {
+            return books = books.OrderBy(x => x.Title).ToList();
+        }
+
+        public List<Book> SortByRating()
+        {
+            return books = books.OrderByDescending(x => x.Rating).ToList();
+        }
+
+        public string[] ProvideInformationAboutAllBooks()
+        {
+            string[] arr = new string[books.Count];
+            for (int i = 0; i < books.Count; i++)
+            {
+                arr[i] = books[i].ToString();
+            }
+            return arr;
+        }
+
+        public bool CheckBookIsInBookLibrary(string title)
+        {
+            for (int i = 0; i < books.Count; i++)
+            {
+                if (books[i].Title == title)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
