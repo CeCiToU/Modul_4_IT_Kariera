503be68b1d718333fe72d2209d0a0527e8246bb2
Tsvetoslav <cveti3060@gmail.com>
2022-07-10 10:47:49 +0300

    second exam

---
+++ IT Kariera Modul 4/ИЗПИТ_2022/SecondExam/Solution/Product.cs
@@ -0,0 +1,42 @@
+﻿namespace Program
+{
+    internal class Product
+    {
+        private string name;
+
+        private double price;
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+           
+
+        }
+
+        public string Name
+
+        {
+
+            get { return name; }
+            set { name = value; }
+
+        }
+
+        public double Price
+
+        {
+
+            get { return price; }
+            set { price = value; }
+
+        }
+
+        public override string ToString()
+        {
+
+            return $"Product {name} costs {price:F2} lv.";
+
+        }
+    }
+}
---
+++ IT Kariera Modul 4/ИЗПИТ_2022/SecondExam/Solution/Supermarket.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Program
+{
+    internal class Supermarket
+    {
+        private string name;
+
+        private List<Product> products = new List<Product>();
+
+        public Supermarket(string name)
+        {
+            Name = name;
+        }
+
+        public string Name
+
+        {
+
+            get { return name; }
+            set { this.name = value; }
+
+        }
+
+        public List<Product> Products
+        {
+            get { return products; }
+            set { products = value; }
+
+
+        }
+
+        public void AddProduct(string name, double price)
+        {
+            Product currentProduct = new Product(name, price);
+            products.Add(currentProduct);
+        }
+
+        public double AveragePriceInRange(double start, double end)
+        {
+            double num = 0;
+            for (int i = 0; i < products.Count; i++)
+            {
+                num += products[i].Price;
+            }
+            num /= products.Count;
+            return num;
+        }
+
+        public List<string> FilterProductsByPrice(double price)
+        {
+            List<string> titles = new List<string>();
+            for (int i = 0; i < products.Count; i++)
+            {
+                if (products[i].Price < price)
+                {
+                    titles.Add(products[i].Name);
+                }
+            }
+            return titles;
+        }
+
+        public List<Product> SortAscendingByName()
+        {
+            return products = products.OrderBy(x => x.Name).ToList();
+        }
+
+        public List<Product> SortDescendingByPrice()
+        {
+            return products = products.OrderByDescending(x => x.Price).ToList();
+        }
+
+        public bool CheckProductIsInSupermarket(string name)
+        {
+            for (int i = 0; i < products.Count; i++)
+            {
+                if (products[i].Name == name)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public string[] ProvideInformationAboutAllProducts()
+        {
+            string[] arr = new string[products.Count];
+            for (int i = 0; i < products.Count; i++)
+            {
+                arr[i] = products[i].ToString();
+            }
+            return arr;
+        }
+    }
+}
